4da85b86503f17b37ae9d061c04bef3566df120e
Jose San Juan <sanjuanjojo@gmail.com>
2015-01-15 22:21:11 +0800

    Edit register_page

--- application/views/include/register_page.php
+++ application/views/include/register_page.php
@@ -1,92 +1,81 @@
-<?php 
-echo'
-				  <div class="panel-body">
-				    <div class="container">
-						<table class="table table-responsive">';
-							echo form_open("pendinguser_controller/register");
-							echo'
-							<tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">Email: </span>';
-									   echo form_input(array("name" => "email", "class" => "form-control", "placeholder" => "Email", "aria-describedby" => "basic-addon1"));
-									   echo'
-									</div>
-								</td>
-							</tr>	
-							<tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">Username: </span>';
-									   echo form_input(array("name" => "username", "class" => "form-control", "placeholder" => "Username", "aria-describedby" => "basic-addon1"));
-									   echo'
-									</div>
-								</td>
-							</tr>	
-							<tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">Password: </span>';
-									   echo form_password(array("name" => "password", "class" => "form-control", "placeholder" => "Password", "aria-describedby" => "basic-addon1"));
-									  echo'
-									</div>
-								</td>
-							</tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">First Name: </span>';
-									   echo form_input(array("name" => "firstname", "class" => "form-control", "placeholder" => "First Name", "aria-describedby" => "basic-addon1"));
-									echo '</div>
-								</td>
-							<tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">Middle Name: </span>';
-									  echo form_input(array("name" => "middlename", "class" => "form-control", "placeholder" => "Middle Name", "aria-describedby" => "basic-addon1"));
-									  echo'
-									</div>
-								</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">Last Name: </span>';
-									  echo form_input(array("name" => "lastname", "class" => "form-control", "placeholder" => "Last Name", "aria-describedby" => "basic-addon1"));
-									  echo'
-									</div>
-								</td>
-							</tr>
-
-							<tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">Address: </span>';
-									  echo form_input(array("name" => "address", "class" => "form-control", "placeholder" => "Address", "aria-describedby" => "basic-addon1"));
-									  echo'
-									</div>
-								</td>
-							</tr>
-
-
-							<tr>
-								<td>
-									<div class="input-group">
-									  <span class="input-group-addon" id="basic-addon1">Date of Term: </span>';
-									  echo form_input(array("name" => "dateofterm", "class" => "form-control", "placeholder" => "2013 - 2014", "aria-describedby" => "basic-addon1"));
-									  echo'
-									</div>
-								</td>
-							</tr>
-						</table>
+<div class="container" style="margin-top:30px; margin-bottom: 30px;">
+<?php echo form_open("pendinguser_controller/register"); ?>
+<div class="row">
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+		<form role="form">
+			<h2>Please Sign Up <small>It's free and always will be.</small></h2>
+			<hr class="colorgraph">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+                        <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" tabindex="1">
 					</div>
-				  </div>
 				</div>
-				<div class="panel panel-info">
-				  <div class="panel-body" align = "center">';
-				    echo form_submit(array("name" => "register", "value" => "Register", "class" => "btn btn-primary"));	
-				    echo form_submit(array("name" => "clear", "value" => "Clear", "class" => "btn btn-primary"));
-				    echo form_close();
-				    echo'
-				  </div>
-				</div>';
- ?>
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+						<input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" tabindex="2">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<input type="text" name="display_name" id="display_name" class="form-control input-lg" placeholder="Display Name" tabindex="3">
+			</div>
+			<div class="form-group">
+				<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+						<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="5">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="6">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-3 col-md-3">
+					<span class="button-checkbox">
+						<button type="button" class="btn" data-color="info" tabindex="7">I Agree</button>
+                        <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
+					</span>
+				</div>
+				<div class="col-xs-8 col-sm-9 col-md-9">
+					 By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="#" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use.
+				</div>
+			</div>
+			
+			<hr class="colorgraph">
+			<div class="row">
+				<div class="col-xs-12 col-md-6"><input type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+				<div class="col-xs-12 col-md-6"><a href="#" class="btn btn-success btn-block btn-lg">Sign In</a></div>
+			</div>
+		</form>
+	</div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="t_and_c_m" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h4 class="modal-title" id="myModalLabel">Terms & Conditions</h4>
+			</div>
+			<div class="modal-body">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary" data-dismiss="modal">I Agree</button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+</div>
