8585bc8f8d2e62e648f594ecdc860230896444dc
Jose San Juan <sanjuanjojo@gmail.com>
2015-01-15 23:14:43 +0800

    register

--- application/views/include/register_page.php
+++ application/views/include/register_page.php
@@ -5,40 +5,44 @@
 		<form role="form">
 			<h2>Please Sign Up <small>It's free and always will be.</small></h2>
 			<hr class="colorgraph">
-			<div class="row">
-				<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-                        <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" tabindex="1">
+				<div class="form-group">
+					<?php echo form_input(array("name" => "firstname","id" => "firstname", "class" => "form-control input-lg", "placeholder" => "First Name", "aria-describedby" => "basic-addon1", "tabindex"=>"1")); ?>
+				</div>
+				<div class="form-group">
+					<?php echo form_input(array("name" => "middlename","id" => "middlename", "class" => "form-control input-lg", "placeholder" => "Middle Name", "aria-describedby" => "basic-addon1", "tabindex"=>"2")); ?>
+				</div>	
+				<div class="form-group">
+					<?php echo form_input(array("name" => "lastname","id" => "lastname", "class" => "form-control input-lg", "placeholder" => "Last Name", "aria-describedby" => "basic-addon1", "tabindex"=>"3")); ?>
+				</div>
+				
+			
+				<div class="form-group">
+					<?php echo form_input(array("name" => "username","id" => "username", "class" => "form-control input-lg", "placeholder" => "Username", "aria-describedby" => "basic-addon1", "tabindex"=>"4")); ?>
+				</div>
+				<div class="form-group">
+					<?php echo form_input(array("name" => "email","id" => "email", "class" => "form-control input-lg", "placeholder" => "Email Address", "aria-describedby" => "basic-addon1", "tabindex"=>"5")); ?>
+				</div>
+				
+				<div class="form-group">
+					<?php echo form_input(array("name" => "address","id" => "address", "class" => "form-control input-lg", "placeholder" => "Address", "aria-describedby" => "basic-addon1", "tabindex"=>"6")); ?>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-6 col-md-6">
+						<div class="form-group">
+							<?php echo form_password(array("name" => "password", "id" => "confirmpassword", "class" => "form-control", "placeholder" => "Password", "aria-describedby" => "basic-addon1", "tabindex" => "7")); ?>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-6">
+						<div class="form-group">
+							<?php echo form_password(array("name" => "confirmpassword", "id" => "confirmpassword", "class" => "form-control", "placeholder" => "Confirm Password", "aria-describedby" => "basic-addon1", "tabindex" => "8")); ?>
+							
+						</div>
 					</div>
 				</div>
-				<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-						<input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" tabindex="2">
-					</div>
-				</div>
-			</div>
-			<div class="form-group">
-				<input type="text" name="display_name" id="display_name" class="form-control input-lg" placeholder="Display Name" tabindex="3">
-			</div>
-			<div class="form-group">
-				<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">
-			</div>
-			<div class="row">
-				<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-						<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="5">
-					</div>
-				</div>
-				<div class="col-xs-12 col-sm-6 col-md-6">
-					<div class="form-group">
-						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="6">
-					</div>
-				</div>
-			</div>
 			<div class="row">
 				<div class="col-xs-4 col-sm-3 col-md-3">
 					<span class="button-checkbox">
-						<button type="button" class="btn" data-color="info" tabindex="7">I Agree</button>
+						<button type="button" class="btn" data-color="info" tabindex="9">I Agree</button>
                         <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
 					</span>
 				</div>
