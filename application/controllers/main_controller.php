5a00e8c066a7c39f2a1051fc5411193a4d90a61b
Jose San Juan <sanjuanjojo@gmail.com>
2015-01-15 16:30:50 +0800

    Create main controller

---
+++ application/controllers/main_controller.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main_Controller extends CI_Controller{
+
+	function __construct() {
+			parent::__construct();
+			$this->load->helper(array('form', 'url', 'html'));
+	}
+
+
+	public function index(){
+		&this->home();
+	}
+
+	public function home(){
+		
+	}
+?>
