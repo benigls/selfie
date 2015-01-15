c1c68ebd95558ba044aceff23f155ef255ab80f2
gidj02 <ginodejesus02@gmail.com>
2015-01-16 03:27:57 +0800

    organized code

--- application/views/content.php
+++ application/views/content.php
@@ -43,143 +43,138 @@
 						<td>'.$records->pending_email.'</td>
 						<td>'.$records->pending_address.'</td>
 						<td>'.$records->pending_date_of_term.'</td>';
-			}
+					}
 
-			echo form_close();
-			echo '	</table>
+					echo form_close();
+					echo '	</table>
 
-				</div>
-				<div class=pull-right>';
+						</div>
+						<div class=pull-right>';
 
-				echo anchor('user_controller/acceptid/'.$records->id.'', 'Accept',(array("class" => "btn btn-primary"))); echo"&nbsp&nbsp"; 
-				echo anchor('user_controller/rejectid/'.$records->id.'', 'Reject',(array("class" => "btn btn-primary"))); 
-				echo'
-				</div>
-				</div>
-			</div>
-			<div style="margin-bottom:70px;"></div>';
+						echo anchor('user_controller/acceptid/'.$records->id.'', 'Accept',(array("class" => "btn btn-primary"))); echo"&nbsp&nbsp"; 
+						echo anchor('user_controller/rejectid/'.$records->id.'', 'Reject',(array("class" => "btn btn-primary"))); 
+						echo'
+						</div>
+						</div>
+					</div>
+					<div style="margin-bottom:70px;"></div>';
 
-			break;	
-
+				break;	
 
 			case "sudoadmin";
-			echo '<div class="container">
-	    
-	    	<div class="row">
-				<div class="col-md-8 col-md-offset-2">
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<h3 class="panel-title">Your Events</h3>
-							<div class="pull-right">
-								<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
-			
-								</span>
+				echo '<div class="container">
+		    
+		    	<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title">Your Events</h3>
+								<div class="pull-right">
+									<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
+									</span>
+								</div>
 							</div>
-						</div>
-						<div class="panel-body">
-							<input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
-						</div>
-						<table class="table table-hover" id="dev-table">
-							<thead>
-								<tr>';
-									echo form_open("user_controller/login");
-									echo '
-									<th>ID</th>
-									<th>Event name</th>
-									<th>Event date</th>
-									<th>Event Description</th>
-									<th>Hashtag</th>';
-				
-							echo '</tr>
-							</thead>
-							<tbody>';
-								foreach($results as $records) {
-								echo'<tr>
-										<td>'.$records->id.'</td>
-										<td>'.$records->event_name.'</td>
-										<td>'.$records->event_date.'</td>
-										<td>'.$records->event_description.'</td>
-										<td>'.$records->hashtag.'</td></tr>';
+							<div class="panel-body">
+								<input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
+							</div>
+							<table class="table table-hover" id="dev-table">
+								<thead>
+									<tr>';
+										echo form_open("user_controller/login");
+										echo '
+										<th>ID</th>
+										<th>Event name</th>
+										<th>Event date</th>
+										<th>Event Description</th>
+										<th>Hashtag</th>';
+					
+								echo '</tr>
+								</thead>
+								<tbody>';
+									foreach($results as $records) {
+									echo'<tr>
+											<td>'.$records->id.'</td>
+											<td>'.$records->event_name.'</td>
+											<td>'.$records->event_date.'</td>
+											<td>'.$records->event_description.'</td>
+											<td>'.$records->hashtag.'</td></tr>';
 
-								}
-							echo '</tbody>
-						</table>
+									}
+								echo '</tbody>
+							</table>
+						</div>
 					</div>
 				</div>
-				
-				
-			</div>
-		</div>';
-		echo form_close();
-		echo anchor("main_controller/addevent/".$id."", "<strong style = 'font-size:35px;'>Add Event</strong>", array("class"=>"navbar-brand"));
-		break;
+			</div>';
+			echo form_close();
+			echo anchor("main_controller/addevent/".$id."", "<strong style = 'font-size:35px;'>Add Event</strong>", array("class"=>"navbar-brand"));
+			break;
+
 		case "addevent_page";
 			echo '<div class="row" style="margin-top:50px;">
-    <div class="col-md-8 col-md-offset-2">';
-      echo form_open("event_controller/add_event/".$id."", array("class"=>"form-horizontal", "role"=>"form"));
-        echo'<fieldset>
+			<div class="col-md-8 col-md-offset-2">';
+			echo form_open("event_controller/add_event/".$id."", array("class"=>"form-horizontal", "role"=>"form"));
+	        echo'<fieldset>
+			          <!-- Form Name -->
+			          <legend>Add Event</legend>
 
-          <!-- Form Name -->
-          <legend>Add Event</legend>
+			          <!-- Text input-->
+			          <div class="form-group">
+			            <label class="col-sm-2 control-label" for="textinput">Name of Event</label>
+			            <div class="col-sm-10">';
+			              echo form_input(array("name" => "name", "class" => "form-control", "placeholder" => "Event Name", "aria-describedby" => "basic-addon1"));
+			             
+			            echo'</div>
+			          </div>
 
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-sm-2 control-label" for="textinput">Name of Event</label>
-            <div class="col-sm-10">';
-              echo form_input(array("name" => "name", "class" => "form-control", "placeholder" => "Event Name", "aria-describedby" => "basic-addon1"));
-             
-            echo'</div>
-          </div>
-
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-sm-2 control-label" for="textinput">Date of Event</label>
-            <div class="col-sm-10">';
-             echo form_input(array("name" => "date", "class" => "form-control", "placeholder" => "YYYY-MM-DD", "aria-describedby" => "basic-addon1"));
-            echo'</div>
-          </div>
-
-
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-sm-2 control-label" for="textinput">Hashtag</label>
-            <div class="col-sm-10">';
-             echo form_input(array("name" => "hashtag", "class" => "form-control","rows"=>"5", "placeholder" => "#HashtagEvemt", "aria-describedby" => "basic-addon1", "style"=>"resize:none;"));
-             echo '
-            </div>
-          </div>
-
-          <!-- Text input-->
-          <div class="form-group">
-            <label class="col-sm-2 control-label" for="textinput">Event Description</label>
-            <div class="col-sm-10">';
-         	 echo form_textarea(array("name" => "eventdesc", "class" => "form-control","rows"=>"5", "placeholder" => "Description of Event", "aria-describedby" => "basic-addon1", "style"=>"resize:none;"));
-             
-            echo '</div>
-          </div>
+			          <!-- Text input-->
+			          <div class="form-group">
+			            <label class="col-sm-2 control-label" for="textinput">Date of Event</label>
+			            <div class="col-sm-10">';
+			             echo form_input(array("name" => "date", "class" => "form-control", "placeholder" => "YYYY-MM-DD", "aria-describedby" => "basic-addon1"));
+			            echo'</div>
+			          </div>
 
 
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-              <div class="pull-right">';
-              	        
-                // echo anchor("event_controller/add_event/".$id."", "<strong style = 'font-size:15px;'>Add Event</strong>", array("class"=>"btn btn-primary", "name" => "add_event"));            
-              	 echo form_submit(array("name" => "add_event", "value" => "Add Event", "class" => "btn btn-primary")); 
-                   echo"&nbsp;&nbsp;";
-                    echo form_submit(array("name" => "clear", "value" => "Clear", "class" => "btn btn-primary")); 
-                    echo form_close();
-    
-              echo'</div>
-            </div>
-          </div>
+			          <!-- Text input-->
+			          <div class="form-group">
+			            <label class="col-sm-2 control-label" for="textinput">Hashtag</label>
+			            <div class="col-sm-10">';
+			             echo form_input(array("name" => "hashtag", "class" => "form-control","rows"=>"5", "placeholder" => "#HashtagEvemt", "aria-describedby" => "basic-addon1", "style"=>"resize:none;"));
+			             echo '
+			            </div>
+			          </div>
 
-        </fieldset>
-      </form>
-    </div><!-- /.col-lg-12 -->
-</div><!-- /.row -->
-<div style="margin-bottom:70px;"></div>';
+			          <!-- Text input-->
+			          <div class="form-group">
+			            <label class="col-sm-2 control-label" for="textinput">Event Description</label>
+			            <div class="col-sm-10">';
+			         	 echo form_textarea(array("name" => "eventdesc", "class" => "form-control","rows"=>"5", "placeholder" => "Description of Event", "aria-describedby" => "basic-addon1", "style"=>"resize:none;"));
+			             
+			            echo '</div>
+			          </div>
+
+
+			          <div class="form-group">
+			            <div class="col-sm-offset-2 col-sm-10">
+			              <div class="pull-right">';
+			              	        
+			                // echo anchor("event_controller/add_event/".$id."", "<strong style = 'font-size:15px;'>Add Event</strong>", array("class"=>"btn btn-primary", "name" => "add_event"));            
+			              	 echo form_submit(array("name" => "add_event", "value" => "Add Event", "class" => "btn btn-primary")); 
+			                   echo"&nbsp;&nbsp;";
+			                    echo form_submit(array("name" => "clear", "value" => "Clear", "class" => "btn btn-primary")); 
+			                    echo form_close();
+			    
+			              echo'</div>
+			            </div>
+			          </div>
+			        </fieldset>
+			      </form>
+			    </div><!-- /.col-lg-12 -->
+			</div><!-- /.row -->
+			<div style=	"margin-bottom:70px;"></div>';
 			break;
-	}	
+	}//end of switch
 
 	if(isset($_POST["tempID"])) echo $_POST["tempID"];
 ?>
