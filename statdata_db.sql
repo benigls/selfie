ca52dfbd8405f9a282412fe4504ab578534d1b8e
James Pogi <jamesv.labnao@gmail.com>
2015-01-15 17:51:45 -0800

    Added pending table

--- statdata_db.sql
+++ statdata_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2015 at 02:30 AM
+-- Generation Time: Jan 16, 2015 at 02:49 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -50,6 +50,24 @@ CREATE TABLE IF NOT EXISTS `event_tbl` (
   `event_description` longtext NOT NULL,
   `hashtag` text NOT NULL,
   PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_user_tbl`
+--
+
+CREATE TABLE IF NOT EXISTS `pending_user_tbl` (
+  `pending_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pending_first_name` text NOT NULL,
+  `pending_middle_name` text NOT NULL,
+  `pending_last_name` text NOT NULL,
+  `pending_password` varchar(64) NOT NULL,
+  `pending_email` text NOT NULL,
+  `pending_address` longtext NOT NULL,
+  `pending_date_of_term` datetime NOT NULL,
+  PRIMARY KEY (`pending_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
